4ebf0501f9c3dc3d4fc50b9ad70a52a6569b3b30
Augustine D. Nguyen <augustine@localhost.localdomain>
2022-02-18 23:21:47 +0700

    Centered array

--- centered-array/src/main/java/com/augustine/miu/App.java
+++ centered-array/src/main/java/com/augustine/miu/App.java
@@ -1,13 +1,54 @@
 package com.augustine.miu;
 
+import java.util.Objects;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        Integer[] input = new Integer[] {1,2,3,4,5};
+        int result = getCenteredArray(input);
+        assert 0 == result;
+        
+        input = new Integer[] {3,2,1,4,5};
+        result = getCenteredArray(input);
+        assert 1 == result;
+
+        input = new Integer[] {3,2,1,4,1};
+        result = getCenteredArray(input);
+        assert 0 == result;
+
+        input = new Integer[] {1,2,3,4};
+        result = getCenteredArray(input);
+        assert 0 == result;
+
+        input = new Integer[] {};
+        result = getCenteredArray(input);
+        assert 0 == result;
+
+        input = new Integer[] {10};
+        result = getCenteredArray(input);
+        assert 1 == result;
+    }
+
+    private static int getCenteredArray(Integer[] input) {
+        if (Objects.isNull(input)) {
+            return 0;
+        }
+
+        if (input.length % 2 == 0) {
+            return 0;
+        }
+
+        int midIndex = input.length / 2;
+        int midValue = input[midIndex];
+        for (int i : input) {
+            if (i <= midValue) {
+                return 0;
+            }
+        }
+        return 1;
     }
 }
