b8f2d1ba602c254ff5b13042abaf6c3c8d65e8b5
augustine <augustine.nguyen@vincere.io>
2022-03-08 10:30:00 +0700

    Add first implementation

--- intersection-array/src/main/java/com/augustine/miu/App.java
+++ intersection-array/src/main/java/com/augustine/miu/App.java
@@ -1,5 +1,8 @@
 package com.augustine.miu;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] result = getIntersection(new int[]{1, 2, 3}, new int[]{4, 3, 1, 9});
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] getIntersection(int[] first, int[] second) {
+        if (Objects.isNull(first) || Objects.isNull(second)) {
+            return new int[0];
+        }
+        if (0 == first.length || 0 == second.length) {
+            return new int[0];
+        }
+        
+        int maxIntersectionCount = first.length > second.length ? second.length : first.length;
+        int[] result = new int[maxIntersectionCount];
+
+        int counter = 0;
+        for (int i = 0; i < second.length; i++) {
+            for (int j = 0; j < first.length; j++) {
+                if (second[i] == first[j]) {
+                    result[counter++] = second[i];
+                }
+            }
+        }
+
+        return result;
     }
 }
