92aaebe64f94598554543680d9cc7c6003855297
augustine <augustine.nguyen@vincere.io>
2022-03-14 16:36:40 +0700

    Add first implementation

--- point-of-equilibrium/src/main/java/com/augustine/miu/App.java
+++ point-of-equilibrium/src/main/java/com/augustine/miu/App.java
@@ -1,5 +1,7 @@
 package com.augustine.miu;
 
+import java.util.Objects;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int result = getPoint(new int[] {3, 1, 1, 2});
+        System.out.println( result);
+    }
+
+    private static int getPoint(int[] a) {
+        if (Objects.isNull(a) || a.length < 3) {
+            return -1;
+        }
+        int sumOfArray = 0;
+        for (int i = 0; i < a.length; i++) {
+            sumOfArray += a[i];
+        }
+
+        int sumOfLeft = 0;
+        for (int i = 1; i < a.length; i++) {
+            sumOfLeft += a[i - 1];
+            if (2 * sumOfLeft == sumOfArray - a[i]) {
+                return i;
+            }
+        }
+        return -1;
+
     }
 }
