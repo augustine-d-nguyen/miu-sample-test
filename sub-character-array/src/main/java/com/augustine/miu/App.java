61f20611fd0345cceec95b929a18b20dd374772d
augustine <augustine.nguyen@vincere.io>
2022-03-16 15:17:23 +0700

    Add draft implementation

--- sub-character-array/src/main/java/com/augustine/miu/App.java
+++ sub-character-array/src/main/java/com/augustine/miu/App.java
@@ -1,5 +1,7 @@
 package com.augustine.miu;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,67 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        char[] result = getChars(new char[]{'a', 'b', 'c'}, 0, 4);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 0, 3);
+        assert Arrays.equals(new char[]{'a', 'b', 'c'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 0, 2);
+        assert Arrays.equals(new char[]{'a', 'b'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 0, 1);
+        assert Arrays.equals(new char[]{'a'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 1, 3);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 1, 2);
+        assert Arrays.equals(new char[]{'b', 'c'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 1, 1);
+        assert Arrays.equals(new char[]{'b'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 2, 2);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 2, 1);
+        assert Arrays.equals(new char[]{'c'}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 3, 1);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, 1, 0);
+        assert Arrays.equals(new char[]{}, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, -1, 2);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{'a', 'b', 'c'}, -1, -2);
+        assert Arrays.equals(null, result);
+
+        result = getChars(new char[]{}, 0, 1);
+        assert Arrays.equals(null, result);
+    }
+
+    private static char[] getChars(char[] a, int start, int len) {
+        if (null == a) {
+            return null;
+        }
+        if (0 > start || a.length <= start) {
+            return null;
+        }
+        if (0 > len || a.length < len) {
+            return null;
+        }
+        if (a.length - start < len) {
+            return null;
+        }
+        int idx = 0;
+        char[] result = new char[len];
+        for (int i = start; i < len; i++) {
+            result[idx++] = a[i];
+        }
+        return result;
     }
 }
