6c57a1cbac474e15ad9cdadec1c404ab14c3e9ce
augustine <augustine.nguyen@vincere.io>
2022-03-16 15:40:29 +0700

    Add draft implementation

--- sum-even-odd/src/main/java/com/augustine/miu/App.java
+++ sum-even-odd/src/main/java/com/augustine/miu/App.java
@@ -8,6 +8,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int result = sum(new int[]{1});
+        assert 1 == result;
+
+        result = sum(new int[]{1,2});
+        assert -1 == result;
+
+        result = sum(new int[]{1,2,3});
+        assert 2 == result;
+
+        result = sum(new int[]{1,2,3,4});
+        assert -2 == result;
+
+        result = sum(new int[]{3,3,4,4});
+        assert -2 == result;
+
+        result = sum(new int[]{3,2,3,4});
+        assert 0 == result;
+
+        result = sum(new int[]{4,1,2,3});
+        assert 2 == result;
+
+        result = sum(new int[]{1,1});
+        assert 2 == result;
+
+        result = sum(new int[]{});
+        assert 0 == result;
+    }
+
+    private static int sum(int[] a) {
+        if (null == a || 0 == a.length) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (0 == a[i] % 2) {
+                result -= a[i];
+            } else {
+                result += a[i];
+            }
+        }
+        return result;
     }
 }
