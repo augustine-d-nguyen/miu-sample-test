92aad904ac80cadd9f666e57bb39017bfbe79eba
augustine <augustine.nguyen@vincere.io>
2022-03-16 14:52:18 +0700

    Add draft implementation

--- reverse-integer/src/main/java/com/augustine/miu/App.java
+++ reverse-integer/src/main/java/com/augustine/miu/App.java
@@ -8,6 +8,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int result = reverse(1234);
+        assert 4321 == result;
+
+        result = reverse(12005);
+        assert 50021 == result;
+
+        result = reverse(1);
+        assert 1 == result;
+
+        result = reverse(1000);
+        assert 1 == result;
+
+        result = reverse(0);
+        assert 0 == result;
+
+        result = reverse(-12345);
+        assert -54321 == result;
+    }
+
+    private static int reverse(int n) {
+        if (10 > n) return n;
+        float result = 0;
+        float mul = 0.1f;
+        int remain = n;
+
+        while (remain > 0) {
+            result += (remain % 10) *mul;
+            remain = remain / 10;
+            mul *= mul;
+        }
+        return (int)(result/mul);
     }
 }
